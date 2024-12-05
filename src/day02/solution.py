8c1668ff98bdeecd8dc22ace5c08bbc63284426d
Joshua Luce <luce@codazed.dev>
2024-12-04 18:09:09 -0600

    Completed Day 02-2

--- src/day02/solution.py
+++ src/day02/solution.py
@@ -1,3 +1,5 @@
+from typing import Iterator
+
 with open("input.txt", "r") as f:
     lines = f.readlines()
 
@@ -27,4 +29,19 @@ def part_01_solution(reports: list[str]) -> int:
             safe_reports += 1
     return safe_reports
 
+def alt_reports(_report: str) -> Iterator[str]:
+    levels = [int(x) for x in _report.split()]
+    for i in range(len(levels)):
+        yield " ".join([str(levels[j]) for j in range(len(levels)) if j != i])
+
+def part_02_solution(reports: list[str]) -> int:
+    safe_reports = 0
+    for report in reports:
+        if report_is_safe(report):
+            safe_reports += 1
+        elif any(report_is_safe(x) for x in alt_reports(report)):
+            safe_reports += 1
+    return safe_reports
+
 print("Part 1 Answer:", part_01_solution(lines))
+print("Part 2 Answer:", part_02_solution(lines))
